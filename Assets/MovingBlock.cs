fdcbbd67a9a9abd5968857aac3ae06a639d0c9b8
Chevy Rios <mv8865pr@go.minnstate.edu>
2021-10-28 15:58:12 -0500

    Added Script to Moving Block
    Moving block will move to the left of the screen creating obstacle for player.

---
+++ Assets/MovingBlock.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingBlock : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 3f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Vector3.left * Time.deltaTime * moveSpeed;
+    }
+}

b6113108e7c853a660fb52ebff1b1455a9482252
Chevy Rios <mv8865pr@go.minnstate.edu>
2021-10-28 19:08:04 -0500

    Update MovingBlock.cs
    Moving Block is now moving with random positions

--- Assets/MovingBlock.cs
+++ Assets/MovingBlock.cs
@@ -5,10 +5,22 @@ using UnityEngine;
 public class MovingBlock : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 3f;
+    float startingYPosition;
+
+    void Start()
+    {
+        startingYPosition = transform.position.y;
+    }
 
     // Update is called once per frame
     void Update()
     {
         transform.position += Vector3.left * Time.deltaTime * moveSpeed;
+        if (transform.position.x <= -15)
+        {
+            transform.position += Vector3.right * 30f;
+            float newY = startingYPosition + UnityEngine.Random.Range(-1f, 1f);
+            transform.position = new Vector3(transform.position.x, newY, 0f);
+        }
     }
 }
